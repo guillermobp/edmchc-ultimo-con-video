0135dfbbf16627a568c04bb89fb37ba771a3a132
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-11-20 21:19:21 -0300

    muestra exponentes del último encuentro en landing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @dias_charlas = Encuentro.last.charlas.group(:fecha).select(:fecha)
     @charlas = Encuentro.last.charlas
+    @exponentes = Encuentro.last.exponentes
   end
 end
