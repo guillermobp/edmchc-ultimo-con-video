afc0473f27ed231369d74ff1079c6596c601e37f
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-11-25 18:25:54 -0300

    administra exponentes

--- app/controllers/exponentes_controller.rb
+++ app/controllers/exponentes_controller.rb
@@ -1,4 +1,9 @@
 class ExponentesController < ApplicationController
+
+  def index
+    @exponentes = Encuentro.last.exponentes
+  end
+
   def new
     @exponente = Exponente.new
   end
@@ -7,9 +12,30 @@ class ExponentesController < ApplicationController
     @exponente = Exponente.new(exponente_params)
     @exponente.encuentro = Encuentro.last
     if (@exponente.save)
-      redirect_to root_path
+      flash[:notice] = 'El exponente ha sido creado exitosamente'
+      redirect_to @exponente
     else
+      flash[:alert] = 'Ocurrió un error intentando crear el exponente'
       redirect_to new_exponente_path
+    end
+  end
+
+  def show
+    @exponente = Exponente.find(params[:id])
+  end
+
+  def edit
+    @exponente = Exponente.find(params[:id])
+  end
+
+  def update
+    @exponente = Exponente.find(params[:id])
+    if @exponente.update(exponente_params)
+      flash[:notice] = 'El exponente ha sido actualizado exitosamente'
+      redirect_to @exponente
+    else
+      flash[:alert] = 'Ocurrió un error intentando actualizar el exponente'
+      redirect_to edit_exponente_path(@exponente)
     end
   end
 
