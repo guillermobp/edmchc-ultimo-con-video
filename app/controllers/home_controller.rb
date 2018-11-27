921455f6543b468edca758de9a263f7b8b6af01b
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-11-27 07:10:47 -0300

    clean: elimina acción para pruebas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,15 +6,4 @@ class HomeController < ApplicationController
     @charlas = Encuentro.last.charlas
     @exponentes = Encuentro.last.exponentes
   end
-
-  def testaws
-    region = 'us-west-2'
-    s3 = Aws::S3::Resource.new(region: region)
-    "holiwi"
-    # list buckets in Amazon S3
-    # s3 = Aws::S3::Client.new
-    # resp = s3.list_buckets
-    # resp.buckets.map(&:name)
-    #=> ["bucket-1", "bucket-2", ...
-  end
 end
