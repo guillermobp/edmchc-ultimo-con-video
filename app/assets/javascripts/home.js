16fb1f270a8af69364fd7e4d7e6950396464de39
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-12-08 21:18:30 -0300

    :bug: verifica presencia de navbar para inicializarla

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,12 +6,14 @@
 
 document.addEventListener("turbolinks:load", function() {
   // Handle navbar item active state.
-  document.querySelector("nav").addEventListener("click", ({ target }) => {
-    document.querySelectorAll("nav a").forEach(a => {
-      a.classList.remove("is-active");
+  if (document.querySelector("nav")) {
+    document.querySelector("nav").addEventListener("click", ({ target }) => {
+      document.querySelectorAll("nav a").forEach(a => {
+        a.classList.remove("is-active");
+      });
+      target.classList.add("is-active");
     });
-    target.classList.add("is-active");
-  });
+  }
 
   var lightbox = new Lightbox();
   lightbox.load();
