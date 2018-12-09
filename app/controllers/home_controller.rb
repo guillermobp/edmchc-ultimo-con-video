63b0f01ed0bd1d4d8db86521c84d5e048925533a
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-12-09 13:57:10 -0300

    :recycle: accede a miembros a través de variable global

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,5 @@ require 'aws-sdk-s3'
 class HomeController < ApplicationController
   def index
     @encuentro = Encuentro.last
-    @dias_charlas = @encuentro.charlas.group(:fecha).select(:fecha)
-    @charlas = @encuentro.charlas
-    @exponentes = @encuentro.exponentes
-    @conciertos = @encuentro.conciertos
   end
 end
